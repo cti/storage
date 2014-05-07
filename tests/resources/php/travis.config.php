1cd00efb396f01c930edc38cdd38d9e3fb25fa52
Nikita.Kuhta <nikita.kuhta@integral.obninsk.ru>
2014-05-07 17:06:52 +0400

    Postgres travis config added

---
+++ tests/resources/php/travis.config.php
@@ -0,0 +1,13 @@
+<?php
+
+return array(
+    // PostgreSQL
+    'Cti\Storage\Adapter\DBAL' => array(
+        'user' => 'postgres',
+        'password' => '',
+        'host' => 'localhost',
+        'port' => '5432',
+        'dbname' => 'cti_storage_tests',
+        'driver' => 'postgres',
+    ),
+);
