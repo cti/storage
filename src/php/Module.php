63d6f3f0a6a15cdd54363c48d5406088633633e4
Dmitry.Krokhin <nekufa@gmail.com>
2014-06-05 10:29:57 +0400

    added core module

---
+++ src/php/Module.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Cti\Storage;
+
+use Build\Application;
+use Cti\Core\Application\Bootloader;
+use Cti\Core\Module\Cache;
+use Cti\Core\Module\Console;
+use Cti\Core\Module\Project;
+
+class Module extends Project implements Bootloader
+{
+
+    /**
+     * @inject
+     * @var Application
+     */
+    public $application;
+
+    public function init(Cache $cache)
+    {
+        parent::init($cache);
+        $this->path = dirname(dirname(__DIR__));
+        $this->prefix = 'Cti\\Storage\\';
+    }
+
+    public function boot(Application $application)
+    {
+        $application->getManager()->getInitializer()->after('Cti\Core\Module\Console', array($this, 'registerCommands'));
+    }
+
+    public function registerCommands(Console $console, Application $application)
+    {
+        foreach($this->getClasses('Command') as $class) {
+            $console->add($this->application->getManager()->get($class));
+        }
+    }
+
+    public function getSchema()
+    {
+        return $this->application->getManager()->get("Cti\\Storage\\Schema");
+    }
+
+    protected function getAvailableNamespaces()
+    {
+        return array('Command');
+    }
+}
