59fc7ed912b024baf222279b753f79e6ae3a2736
Dmitry.Krokhin <nekufa@gmail.com>
2014-06-18 14:17:27 +0400

    rollback model generator

---
+++ src/php/Generator/Model.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Cti\Storage\Generator;
+
+class Model
+{
+    /**
+     * @inject
+     * @var \Cti\Core\Module\Fenom
+     */
+    protected $fenom;
+
+    /**
+     * @var \Cti\Storage\Component\Model
+     */
+    protected $model;
+
+    /**
+     * @inject
+     * @var \Cti\Storage\Schema
+     */
+    protected $schema;
+
+    public function getCode()
+    {
+        $code = $this->fenom->render('model', array(
+            'model' => $this->model,
+            'schema' => $this->schema,
+            'generator' => $this,
+        ));
+        return $code;
+    }
+
+    /**
+     * @param \Cti\Storage\Component\Model $linkModel
+     */
+    public function getOppositeModel($linkModel)
+    {
+        foreach($linkModel->getReferences() as $reference) {
+            if ($reference->getDestination() != $this->model->getName()) {
+                return $this->schema->getModel($reference->getDestination());
+            }
+        }
+    }
+
+
+}
